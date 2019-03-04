18cdbcf4280ffee1724496c0d397924a19627886
AntonioDell <Antonio.Dell@gmx.de>
2019-03-04 20:42:15 +0100

    Fix stuff

--- server.mjs
+++ server.mjs
@@ -7,7 +7,7 @@ let app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-app.listen(80, function () {
+app.listen(3000, function () {
     console.log("Example app listening on port 3000!");
 });
 
